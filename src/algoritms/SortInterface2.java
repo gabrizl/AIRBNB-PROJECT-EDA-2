0f281035b688c3e5ae9207d610689890cb0d5cea
gabrizl <gsilva.pb@gmail.com>
2022-03-26 23:06:11 -0300

    first commit

---
+++ src/algoritms/SortInterface2.java
@@ -0,0 +1,7 @@
+package algoritms;
+
+import java.util.ArrayList;
+
+public interface SortInterface2 {
+  ArrayList<String[]> sort(ArrayList<String[]> matrix, int colomunIndex, String pathToSaveMetrics);
+}
---
+++ src/algoritms/TransformInterface2.java
@@ -0,0 +1,8 @@
+package algoritms;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public interface TransformInterface2 {
+    public ArrayList<ArrayList<String>> transform(ArrayList<ArrayList<String>> matrix, int indexCol, String savePath) throws ParseException;
+}
